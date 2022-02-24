8b03ffcb32461acf47df1c2facb71e0423ca213a
changhoi <0417kch@naver.com>
2022-02-24 18:19:54 +0900

    boj 2143

--- BOJ/P2143/Main.java
+++ BOJ/P2143/Main.java
@@ -1,105 +1,87 @@
-package DAY02.P2143;
+package P2143;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int[] A, B;
+    static List<Long> sumA = new ArrayList<>(), sumB = new ArrayList<>();
 
-    static long T;
-    static int N, M;
-    static long[] inputA, inputB;
+    static void init(List<Long> sArr, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            long sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                sArr.add(sum);
+            }
+        }
+
+        sArr.sort(null);
+    }
 
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src\\DAY02\\P2143\\input.txt"));
-
+        System.setIn(new FileInputStream("src/P2143/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        T = Long.parseLong(br.readLine());
-        N = Integer.parseInt(br.readLine());
-        inputA = new long[N];
+        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        A = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            inputA[i] = Long.parseLong(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        M = Integer.parseInt(br.readLine());
-        inputB = new long[M];
+        int m = Integer.parseInt(br.readLine());
+
+        B = new int[m];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            inputB[i] = Long.parseLong(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(Arrays.toString(inputA));
-        System.out.println(Arrays.toString(inputB));
+        init(sumA, A);
+        init(sumB, B);
 
-        List<Long> subA = new ArrayList<>();
-        List<Long> subB = new ArrayList<>();
+        long ans = 0;
 
-        for (int i = 0; i < N; i++) {
-            long sum = inputA[i];
-            subA.add(sum);
-            for (int j = i + 1; j < N; j++) {
-                sum += inputA[j];
-                subA.add(sum);
+        int l = 0, r = sumB.size() - 1;
+        while (l < sumA.size() && r >= 0) {
+            long aValue = sumA.get(l);
+            long bValue = sumB.get(r);
+            long v = aValue + bValue;
+            if (v > t) r--;
+            else if (v < t) l++;
+            else {
+                long cnt1 = 1, cnt2 = 1;
+                while (l < sumA.size() - 1) {
+                    long l1 = sumA.get(l);
+                    long l2 = sumA.get(l + 1);
+                    if (l1 != l2) break;
+                    l++;
+                    cnt1++;
+                }
+
+                while (r > 0) {
+                    long r1 = sumB.get(r);
+                    long r2 = sumB.get(r - 1);
+                    if (r1 != r2) break;
+                    r--;
+                    cnt2++;
+                }
+
+                ans += cnt1 * cnt2;
+                l++;
+                r--;
             }
+
         }
 
-//      System.out.println(subA);
-
-        for (int i = 0; i < M; i++) {
-            long sum = inputB[i];
-            subB.add(sum);
-            for (int j = i + 1; j < M; j++) {
-                sum += inputB[j];
-                subB.add(sum);
-            }
-        }
-
-//      Collections.sort(subA);
-        Collections.sort(subB);
-
-        long result = 0;
-
-        for (int i = 0; i < subA.size(); i++) {
-            int lower = lowerBound(subB, 0, subB.size(), T - subA.get(i));
-            int upper = upperBound(subB, 0, subB.size(), T - subA.get(i));
-            result += (upper - lower);
-        }
-        System.out.println(result);
-    }
-
-    static int upperBound(List<Long> list, int left, int right, long target) {
-        int mid = 0;
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (list.get(mid) <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return right;
-    }
-
-    static int lowerBound(List<Long> list, int left, int right, long target) {
-        int mid = 0;
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (list.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return right;
+        System.out.println(ans);
     }
 }
